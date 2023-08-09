83a37c786c51d1d0cd8377a38ac35cd05df0081b
Matt Stokes <103947423+RatherStoked@users.noreply.github.com>
2023-08-09 14:47:36 -0700

    Update InputOutput_CinCout.cpp

--- InputOutput_CinCout.cpp
+++ InputOutput_CinCout.cpp
@@ -1,21 +1,14 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-using namespace std;
-
 
 int main() {
-    int a;
-    int b;
-    int c;
+    int a, b, c;
     
-    cin >> a >> b >> c;
+    std::cout << "Enter three integers: ";
+    std::cin >> a >> b >> c;
     
-    int result = a + b + c;
+    int sum = a + b + c;
     
-    cout << result;
+    std::cout << "Sum: " << sum << std::endl;
 
     return 0;
 }
