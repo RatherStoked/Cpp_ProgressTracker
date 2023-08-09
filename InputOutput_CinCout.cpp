d6dddb1af531edb5879f5d7af381a95e6343f626
Matt Stokes <103947423+RatherStoked@users.noreply.github.com>
2023-08-09 12:07:25 -0700

    Create InputOutput_CinCout.cpp
    Using Cin and Cout for a basic input output function.

---
+++ InputOutput_CinCout.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    
+    cin >> a >> b >> c;
+    
+    int result = a + b + c;
+    
+    cout << result;
+
+    return 0;
+}
